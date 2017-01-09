9c3597fd3709e531571c5ab5fe1303f787a17777
Lucy Fox <lucyreneefox@gmail.com>
2017-01-09 16:46:09 -0700

    Set up email preview sampler

--- app/mailers/example_mailer.rb
+++ app/mailers/example_mailer.rb
@@ -1,2 +1,8 @@
 class ExampleMailer < ApplicationMailer
+  default from: "lucyfox80@gmail.com"
+
+  def sample_email(user)
+    @user = user
+    mail(to: @user.email, subject: 'Sample Email')
+  end
 end
